0bdd41015ccf7f084de2d48889a4f9181298a06b
Hatamli <kamilahat@code.edu.az>
2022-03-17 11:24:03 +0400

    Add my first project to GitHub

---
+++ K8/Ədədin mərtəbələrinin sayını tapan algoritm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ədədin_mərtəbələrinin_sayını_tapan_algoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7878;
+            int floor = 0;
+
+            while (a > 0)
+            {
+                a /= 10;
+                floor++;
+            }
+
+            Console.WriteLine(floor);
+
+        }
+    }
+}
